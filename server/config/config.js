fae7f8ab34b256c366a61dfce0a57c4a901d3e03
Ivan <ivancabeza85@gmail.com>
2020-03-12 23:18:46 +0100

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -36,3 +36,9 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+
+// ==========================
+// Google Client ID
+// ==========================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '154102253880-e8o0cu4jbvoket6ng6ilbpt52qs5h526.apps.googleusercontent.com';
